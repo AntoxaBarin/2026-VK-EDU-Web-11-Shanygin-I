cbe776b4c0bbbaeb4105d603b1d19efcbc1efd47
Ivan Shanygin <shanygin101@yandex.ru>
2026-03-29 00:55:13 +0300

    Done

--- questions/urls.py
+++ questions/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("hot/", views.hot, name="hot"),
-    path("question/", views.question, name="question"),
     path("ask/", views.ask, name="ask"),
-    path("tag/", views.tag, name="tag"),
+    path("question/<int:question_id>/", views.question, name="question"),
+    path("tag/<str:tag_name>/", views.tag, name="tag"),
 ]
